fe6039724d075934f13ea82316e19ef07abdd117
naya86 <80315799+naya86@users.noreply.github.com>
2021-04-28 00:18:01 +0900

    add

--- ssd_model_video.py
+++ ssd_model_video.py
@@ -100,9 +100,9 @@ def show_inference(model, image_np):
         use_normalized_coordinates=True,
         line_thickness=8)
 
-    image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+    # image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
     # image_np = cv2.resize( image_np, (800,600), interpolation=cv2.INTER_AREA)
-    st.video(image_np)
+    st.image(image_np)
 
 
 
@@ -117,32 +117,34 @@ def ssd_video() :
     # st.video(cap)
     # if cap.isOpened() == False :    # True False로 값이 나옴 isOpened
     # print('Error opening video stream of file')
-    
+    image_placeholder = st.empty()
     # else :
     # # 반복문 필요이유 : 비디오는 여러 사진으로 구성되어 있으니까.! 여러개니까
-    # while cap.isOpened() :
+    while cap.isOpened() :
             
     #         # 사진을 한장씩 가져와서 
 
-    ret, frame = cap.read()       # ret 에는 True , False 로 가져오고 , frame 에는 numpy 로 가져옴(이미지). 비디오에 관한 프레임이 있으면 ret은 True
+        ret, frame = cap.read()       # ret 에는 True , False 로 가져오고 , frame 에는 numpy 로 가져옴(이미지). 비디오에 관한 프레임이 있으면 ret은 True
 
-    #         # 제대로 사진 가져왔으면, 화면에 표시
-    if ret == True :
-    #             # 이 부분을 모델 추론, 화면에 보여주는 코드로 변경
-                # image_placeholder.image(frame, channels=)      
-    #             # cv2.imshow('frame', frame)
-    #             start_time = time.time() # 추론시간 계산.
-        show_inference(detection_model, frame)     #  가공이 필요할때는 이 부분에 가공을 해주면 된다.
-    #             end_time = time.time()
-    #             #print(end_time - start_time)
+        #         # 제대로 사진 가져왔으면, 화면에 표시
+        if ret == True :
+            # image_placeholder.image(frame, channels='BGR')  
+        #           # 이 부분을 모델 추론, 화면에 보여주는 코드로 변경
+                    # image_placeholder.image(frame, channels=)      
+            # cv2.imshow('frame', frame)
+        #             start_time = time.time() # 추론시간 계산.
+            # image_placeholder(frame, channels="BGR")
+            # show_inference(detection_model, frame)     #  가공이 필요할때는 이 부분에 가공을 해주면 된다.
+        #             end_time = time.time()
+        #             #print(end_time - start_time)
 
-    #             #키보드에서 esc키를 누르면 exit 하라
-    #             if cv2.waitKey(25) & 0xFF == 27 :
-    #                 break
+        #             #키보드에서 esc키를 누르면 exit 하라
+        #             if cv2.waitKey(25) & 0xFF == 27 :
+        #                 break
 
-    #         else : 
-    #             break
+        else : 
+            break
 
-    #     cap.release()  # 비디오파일 닫는 느낌
+    cap.release()  # 비디오파일 닫는 느낌
 
     #     cv2.destroyAllWindows()
