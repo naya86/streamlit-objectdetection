9a316da3a343b62f548781d8b220ed8bfacb26f7
naya86 <80315799+naya86@users.noreply.github.com>
2021-04-29 12:33:42 +0900

    add

--- home.py
+++ home.py
@@ -11,7 +11,14 @@ def run_home() :
     st.write('자율주행과 관련이 있고 , 그것이 아니더라도 모든 분야에서 사물을 인식하는 기술은')
     st.write('접목되고 있어 중요한 기술로 자리 잡고 있다.')
 
-    st.write('이번 프로젝트는 각 모델의 이해와 이미지 및 영상 Detection , Segmentation이다.')
+    home_od_img = Image.open('data/images/object_detection2.PNG')
+
+    st.image(home_od_img,width=800)
+
+    st.write('위는 Object Detection 모델의 발전과정을 나타내고 있고, 초기모델부터 현재까지 처리 속도 및 정확도를 향상하며')
+    st.write('계속 발전되며 개발되고 있다.')
+
+    st.write('이번 프로젝트는 모델의 이해와 이미지 및 영상 Detection , Segmentation을 직접 처리한다.')
     
     img = Image.open('data/images/home_image.png')
     st.image(img,width=800)
--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -10,9 +10,37 @@ from input_image import input_image_seg
 
 
 def run_seg() : 
-   input_image_seg()
    
+   menu = ['About Semantic Segmentation', 'E-NET project']
 
+   select = st.sidebar.selectbox('SELECT', menu)
+   # input_image_seg()
+   
+   if select == 'About Semantic Segmentation' :
+        pass
+
+   elif select == 'E-NET project' :
+    
+      sel_seg = st.radio('Select', ['Input Image','Video'] )
+
+      
+      if sel_seg == 'Input Image' : 
+         input_image_seg()
+            
+        
+        
+        
+        
+      if sel_seg == 'Video' :
+
+         st.subheader('출력되는 영상 실시간 Object Dection')
+                    
+           
+         video_file = open('data/videos/output_seg.mp4', 'rb').read()
+         # video_file = cv2.cvtColor(video_file, cv2.COLOR_BGR2RGB)
+         st.video(video_file)
+
+         st.write('위의 화면은 프리티어 EC2 특성상, 로컬에서 작업하여 나온 결과물을 올린 영상이다.')
 
 
 
